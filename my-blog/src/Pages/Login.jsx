6ecd22dd4e03e1f6a04bf2854f39d1bde49299ee
parthdeshmukh2 <parthdeshmukh99@gmail.com>
2022-12-21 07:41:09 +0530

    login and signup completed

--- my-blog/src/Pages/Login.jsx
+++ my-blog/src/Pages/Login.jsx
@@ -1,12 +1,84 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
+import React from "react";
+import { Box, Input, Text, Button } from "@chakra-ui/react";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { userLogin } from "../Redux/AuthReducer/action";
 
 const Login = () => {
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  // Dispatching the Login Function to Redux/AuthReducer/action.js
+  // This function will only called when userName and password has a valid input
+  // You have to fill the correct details as per registration only then it will login
+  const handleLogin = () => {
+    if (Email && Password) {
+      dispatch(userLogin({ Email, Password }, navigate));
+    } else {
+      alert("Please fill both the details");
+    }
+  };
+
   return (
     <Box>
-      
-    </Box>
-  )
-}
+      <Navbar />
+      <Box
+        w={{ base: "90%", md: "60%", lg: "30%" }}
+        display="flex"
+        flexDirection="column"
+        p="4"
+        m="auto"
+        mt="16"
+        alignItems="center"
+        boxShadow="2xl"
+        rounded="xl"
+      >
+        <Text fontSize="2xl" fontWeight="700" fontFamily="cursive">
+          Login
+        </Text>
+        <Input
+          placeholder="Enter Email"
+          type="text"
+          mt="4"
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-export default Login
+        <Input
+          placeholder="Enter UserName"
+          type="password"
+          mt="4"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <Button bg="green" color="white" mt="8" onClick={handleLogin}>
+          Login
+        </Button>
+
+        <Box display="flex" mt="8">
+          <Text fontSize="lg" fontFamily="cursive">
+            Don't have an Account?
+          </Text>
+          <Text
+            fontSize="lg"
+            fontFamily="cursive"
+            color="blue"
+            textDecoration="underline"
+            cursor="pointer"
+            onClick={() => navigate("/register")}
+          >
+            Register Here
+          </Text>
+        </Box>
+      </Box>
+      <Footer />
+    </Box>
+  );
+};
+
+export default Login;
--- my-blog/src/Pages/SignUp.jsx
+++ my-blog/src/Pages/SignUp.jsx
@@ -1,12 +1,95 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from "react";
+import { Box, Input, Text, Button } from "@chakra-ui/react";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../Redux/AuthReducer/action";
 
 const SignUp = () => {
+  const [Email, setEmail] = useState("");
+  const [Name, setName] = useState("");
+  const [LastName, setLastName] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  // Dispatching the Register function to Redux/AuthReducer/Register/
+  // This function will only run when userName and password has a valid input
+  const handleSignUp = () => {
+    if (Email && Name && LastName && Password) {
+      dispatch(registerUser({ Email, Name, LastName, Password }, navigate));
+    }
+  };
+
   return (
     <Box>
-      
-    </Box>
-  )
-}
+      <Navbar />
+      <Box
+        w={{ base: "90%", md: "60%", lg: "30%" }}
+        display="flex"
+        flexDirection="column"
+        p="4"
+        m="auto"
+        mt="16"
+        alignItems="center"
+        boxShadow="2xl"
+        rounded="xl"
+      >
+        <Text fontSize="2xl" fontWeight="700" fontFamily="cursive">
+          Register
+        </Text>
+        <Input
+          placeholder="Enter Name"
+          type="text"
+          mt="4"
+          onChange={(e) => setName(e.target.value)}
+        />
 
-export default SignUp
+        <Input
+          placeholder="Enter LastName"
+          type="text"
+          mt="4"
+          onChange={(e) => setLastName(e.target.value)}
+        />
+
+        <Input
+          placeholder="Enter Email"
+          type="text"
+          mt="4"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <Input
+          placeholder="Enter Password"
+          type="password"
+          mt="4"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <Button bg="green" color="white" mt="8" onClick={handleSignUp}>
+          Register
+        </Button>
+
+        <Box display="flex" mt="8">
+          <Text fontSize="lg" fontFamily="cursive">
+            Already have an Account?
+          </Text>
+          <Text
+            fontSize="lg"
+            fontFamily="cursive"
+            color="blue"
+            textDecoration="underline"
+            cursor="pointer"
+          >
+            Login Here
+          </Text>
+        </Box>
+      </Box>
+      <Footer />
+    </Box>
+  );
+};
+
+export default SignUp;
